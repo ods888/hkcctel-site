deb70511754ca53c3ca43a932bb546df594c0167
CCTel Deploy <deploy@hkcctel.com>
2026-07-06 00:39:12 +0800

    Harden static deploy and update homepage visual

--- data/fix_featured.py
+++
@@ -1,5 +0,0 @@
-import json
-d = json.load(open('/sessions/intelligent-upbeat-mccarthy/mnt/hkcctel/data/products.json'))
-d[8]['featured'] = True
-json.dump(d, open('/sessions/intelligent-upbeat-mccarthy/mnt/hkcctel/data/products.json', 'w'), indent=2, ensure_ascii=False)
-print(f'[{d[8]["id"]}] {d[8]["name"]} → FEATURED')
